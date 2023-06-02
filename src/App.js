1acf00a9f66dab2bd5c60e5404dbe895e1dc5957
jkim000 <joshuakim514@gmail.com>
2023-06-02 14:01:58 -0700

    refactor: update rq component with react-query version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import { HomePage } from "./components/Home.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
@@ -7,32 +8,41 @@ import "./App.css";
 function App() {
     // this queryClient combined with the Provider at the top level of the jsx
     // gives all children access to the hooks react-query provides
+    const queryClient = new QueryClient();
+
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/super-heroes">TradSuperheroes</Link>
-                        </li>
-                        <li>
-                            <Link to="/rq-super-heroes">RQ Superheroes</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/super-heroes" element={<SuperHeroesPage />} />
-                    <Route
-                        path="/rq-super-heroes"
-                        element={<RQSuperHeroesPage />}
-                    />
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </div>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/super-heroes">TradSuperheroes</Link>
+                            </li>
+                            <li>
+                                <Link to="/rq-super-heroes">
+                                    RQ Superheroes
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route
+                            path="/super-heroes"
+                            element={<SuperHeroesPage />}
+                        />
+                        <Route
+                            path="/rq-super-heroes"
+                            element={<RQSuperHeroesPage />}
+                        />
+                        <Route path="/" element={<HomePage />} />
+                    </Routes>
+                </div>
+            </Router>
+        </QueryClientProvider>
     );
 }
 
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,6 +1,29 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
 /**
  * Using react-query instead of the more traditional way without it
  */
+
+const fetchSuperHeroes = () => {
+    return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-    return <div>RQSuperHeroes Page</div>;
+    // first arg is a unique key for this useQuery
+    // second arg is a fn that returns a promise (notice the fetch is done outside the component)
+    const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <>
+            <h2>RQ Superheroes Page</h2>
+            {data?.data.map((hero) => {
+                return <div key={hero.alterEgo}>{hero.alterEgo}</div>;
+            })}
+        </>
+    );
 };
