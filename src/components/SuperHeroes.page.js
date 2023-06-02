ce1f0ff1aa94d274755ea835a7c402f859693094
jkim000 <joshuakim514@gmail.com>
2023-06-02 13:44:26 -0700

    refactor: get data from db and display in SuperHeroes component

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,3 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const SuperHeroesPage = () => {
-    return <div>SuperHeroes Page</div>;
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/superheroes").then((res) => {
+            setData(res.data);
+            setIsLoading(false);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <>
+            <h2>Superheroes Page</h2>
+            {data.map((hero) => (
+                <div key={hero.name}>{hero.name}</div>
+            ))}
+        </>
+    );
 };
