c8fb31422eb389d0ed9037ed64ba59b5f46feea1
jkim000 <joshuakim514@gmail.com>
2023-06-02 13:02:25 -0700

    refactor: initial App component layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HomePage } from "./components/Home.page";
+import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/super-heroes">TradSuperheroes</Link>
+                        </li>
+                        <li>
+                            <Link to="/rq-super-heroes">RQ Superheroes</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/super-heroes" element={<SuperHeroesPage />} />
+                    <Route
+                        path="/rq-super-heroes"
+                        element={<RQSuperHeroesPage />}
+                    />
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
