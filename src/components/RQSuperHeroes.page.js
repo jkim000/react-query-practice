ff03c134446d462e3f6adbe593f46ca7cc1a8f36
jkim000 <joshuakim514@gmail.com>
2023-06-02 18:57:23 -0700

    chore: notes on various useQuery options

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
@@ -12,12 +13,32 @@ const fetchSuperHeroes = () => {
 export const RQSuperHeroesPage = () => {
     // first arg is a unique key for this useQuery
     // second arg is a fn that returns a promise (notice the fetch is done outside the component)
-    const { isLoading, data, isError, error } = useQuery(
+    // options include...
+    // { cacheTime: timeInMs } (default 5 min) data stays cached for time frame, fetches from cache instead of api within timeframe
+    // { staleTime: timeInMs } (default 0 sec) data stays fresh for timeframe, app won't re-fetch even from cache within timeframe, but will if past the timeframe
+    // { refetchOnMount: boolean } (default true) re-fetches everytime on component mount
+    // { refetchOnWindowFocus: boolean or 'always' } (default false) re-fetches every time on window focus
+    // { refetchInterval: false or timeInMs } (default false) refetch query at listed interval, pauses on window losing focus
+    // { refetchIntervalInBackground: false or timeInMs } (default false) refetch query at listed interval even in background
+    // { enabled: false } turns off the useQuery
+    // can enable fetch onClick using refetch from the destructured refetch var in useQuery, subject to all cache and staleTime
+    // { onSuccess: fn(data) } handle side effects on success
+    // { onError: fn(error) } handle side effects on error
+    // { select: (data) => return transformedData } select receives the data as an arg. you can then do what you want with the data like transform or filter.
+    const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
         "super-heroes",
-        fetchSuperHeroes
+        fetchSuperHeroes,
+        {
+            select: (data) => {
+                const onlyAlterEgoNames = data?.data.map(
+                    (hero) => hero.alterEgo
+                );
+                return onlyAlterEgoNames;
+            },
+        }
     );
 
-    if (isLoading) {
+    if (isLoading || isFetching) {
         return <h2>Loading...</h2>;
     }
 
@@ -28,8 +49,8 @@ export const RQSuperHeroesPage = () => {
     return (
         <>
             <h2>RQ Superheroes Page</h2>
-            {data?.data.map((hero) => {
-                return <div key={hero.alterEgo}>{hero.alterEgo}</div>;
+            {data?.map((hero) => {
+                return <div key={hero}>{hero}</div>;
             })}
         </>
     );
